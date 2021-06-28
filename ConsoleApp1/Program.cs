09e8f68a7f58756b1b6a230a2ba875ea5ae5a263
Charm1nk <mry2002@mail.ru>
2021-06-28 21:20:18 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            string input = String.Empty;
+            input = Console.ReadLine();
+            string[] censoredWords = {"капибарами", "капибарой", "капибарах", "капибарам", "капибара", 
+                "капибары", "капибаре", "капибару", "капибар"};
+            foreach(string censoredWord in censoredWords)
+            {
+                int index = input.IndexOf(censoredWord, StringComparison.CurrentCultureIgnoreCase);
+                if (index != -1)
+                {
+                    input = input.Replace(censoredWord, new string('*', censoredWord.Length), 
+                        StringComparison.CurrentCultureIgnoreCase);
+                } 
+            }
+            Console.WriteLine(input);
+
+            
+        }
+    }
+}
